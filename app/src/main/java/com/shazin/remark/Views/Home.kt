d05be2cca6288b74fe16e218848b439e9f9785cf
Shazin <shazin.bd@gmail.com>
2024-02-26 14:52:40 +0600

    Updated app/src/main/java/com/shazin/remark/Views/Home.kt;

--- app/src/main/java/com/shazin/remark/Views/Home.kt
+++ app/src/main/java/com/shazin/remark/Views/Home.kt
@@ -1,10 +1,22 @@
 package com.shazin.remark.Views
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(navHostController: NavHostController){
-    Text(text = "Yo")
+    Scaffold {paddingValues ->
+        Box(modifier =
+            Modifier.fillMaxSize()
+                .padding(paddingValues)
+        )
+    }
 }
