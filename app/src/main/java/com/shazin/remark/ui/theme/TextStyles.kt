76630e5b49a726b5a7224d6015bddc95b2f626bd
Shazin <shazin.bd@gmail.com>
2024-02-26 22:25:22 +0600

    Updated app/src/main/java/com/shazin/remark/ui/theme/TextStyles.kt;

--- app/src/main/java/com/shazin/remark/ui/theme/TextStyles.kt
+++ app/src/main/java/com/shazin/remark/ui/theme/TextStyles.kt
@@ -1,7 +1,9 @@
 package com.shazin.remark.ui.theme
 
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 
-val dspLarge = TextStyle(
+val displayLarge = TextStyle(
+    fontFamily = ProductSans,
 
 )
