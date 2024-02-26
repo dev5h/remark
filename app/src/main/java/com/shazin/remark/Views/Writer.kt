808424ff9998ad19c189c103c7b8ed48e45aa8a5
Shazin <shazin.bd@gmail.com>
2024-02-26 22:45:39 +0600

    Updated app/src/main/java/com/shazin/remark/Views/Writer.kt;

--- app/src/main/java/com/shazin/remark/Views/Writer.kt
+++ app/src/main/java/com/shazin/remark/Views/Writer.kt
@@ -1,10 +1,17 @@
 package com.shazin.remark.Views
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
 @Composable
 fun Writer(navHostController: NavHostController){
-    Text(text = "Editor")
+    Scaffold {paddingValues ->
+        Box(modifier = Modifier.fillMaxSize().padding(paddingValues))
+    }
 }
