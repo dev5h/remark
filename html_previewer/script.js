5964051d152a8e895240b380fd9990bc60569b1b
Shazin <shazin.bd@gmail.com>
2024-02-27 02:29:23 +0600

    Updated html_previewer/index.html; Added html_previewer/script.js;

---
+++ html_previewer/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const md = window.markdownit();
+  document.getElementById("container").innerHTML = md.render(
+    document.getElementById("container").innerText
+  );
+
+  renderMathInElement(document.body, {
+    // customised options
+    // • auto-render specific keys, e.g.:
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true },
+    ],
+    // • rendering keys, e.g.:
+    throwOnError: false,
+  });
+});
