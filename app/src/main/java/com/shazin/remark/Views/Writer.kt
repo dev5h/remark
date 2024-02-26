57c385bf9e22fc49bddb15b4446e5f663bb0033b
Shazin <shazin.bd@gmail.com>
2024-02-26 22:53:14 +0600

    Updated app/src/main/java/com/shazin/remark/Views/Writer.kt;

--- app/src/main/java/com/shazin/remark/Views/Writer.kt
+++ app/src/main/java/com/shazin/remark/Views/Writer.kt
@@ -1,5 +1,6 @@
 package com.shazin.remark.Views
 
+import android.webkit.WebView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,3 +38,13 @@ fun Writer(navHostController: NavHostController){
     }
 }
 
+@Composable
+fun WebViewScreen(){
+    AndroidView(
+        factory = {context->
+            WebView(context).apply {
+                settings.
+            }
+        }
+    )
+}
