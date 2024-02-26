76418e33dd519d427b15b8c7fc01811c7006b230
Shazin <shazin.bd@gmail.com>
2024-02-26 15:15:23 +0600

    Updated app/src/main/java/com/shazin/remark/Widgets/NoNoteFoundDisplay.kt;

--- app/src/main/java/com/shazin/remark/Widgets/NoNoteFoundDisplay.kt
+++ app/src/main/java/com/shazin/remark/Widgets/NoNoteFoundDisplay.kt
@@ -1,4 +1,25 @@
 package com.shazin.remark.Widgets
 
-class EmptyNoteDisplay {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun NoNoteFound(navHostController: NavHostController){
+    Column {
+        Text(text = "No notes found")
+        Button(onClick = { /*TODO*/ }) {
+            Row {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Create Icon")
+                Text(text = "Create")
+            }
+
+        }
+    }
 }
