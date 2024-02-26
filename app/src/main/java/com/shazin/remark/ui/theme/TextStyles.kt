dee984fcd0d987ef7f8d83318403baeac14a4058
Shazin <shazin.bd@gmail.com>
2024-02-26 22:30:26 +0600

    Updated app/src/main/java/com/shazin/remark/ui/theme/TextStyles.kt;

--- app/src/main/java/com/shazin/remark/ui/theme/TextStyles.kt
+++ app/src/main/java/com/shazin/remark/ui/theme/TextStyles.kt
@@ -1,13 +1,15 @@
 package com.shazin.remark.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose
 
 val displayLarge = TextStyle(
     fontFamily = ProductSans,
     fontWeight = FontWeight.Normal,
-    fontSize = Typography.displayLarge.fontSize,
+    fontSize = ,
     lineHeight = Typography.displayLarge.lineHeight,
     letterSpacing = Typography.displayLarge.letterSpacing
 
