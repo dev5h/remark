45008d19bcfdaed588f213bafa655f452ac0a8e7
Shazin <shazin.bd@gmail.com>
2024-02-26 21:42:34 +0600

    Updated app/src/main/java/com/shazin/remark/ui/theme/Type.kt, app/src/main/res/font/product_sans.xml; Renamed app/src/main/res/font/ProductSans-Regular.ttf -> app/src/main/res/font/ProductSans_Regular.ttf;

--- app/src/main/java/com/shazin/remark/ui/theme/Type.kt
+++ app/src/main/java/com/shazin/remark/ui/theme/Type.kt
@@ -6,6 +6,10 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+
+// Product Sans font
+val ProductSans = FontFamily()
+
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
@@ -15,6 +19,7 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
