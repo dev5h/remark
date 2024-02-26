033e3c5f5459ab97cbceb8c78448a3827412c5a0
Shazin <shazin.bd@gmail.com>
2024-02-26 22:48:10 +0600

    Updated app/src/main/java/com/shazin/remark/Views/Writer.kt;

--- app/src/main/java/com/shazin/remark/Views/Writer.kt
+++ app/src/main/java/com/shazin/remark/Views/Writer.kt
@@ -3,15 +3,35 @@ package com.shazin.remark.Views
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Writer(navHostController: NavHostController){
-    Scaffold {paddingValues ->
-        Box(modifier = Modifier.fillMaxSize().padding(paddingValues))
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { /*TODO*/ },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back button" )
+                    }
+                }
+            )
+        }
+    ) {paddingValues ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues))
     }
 }
