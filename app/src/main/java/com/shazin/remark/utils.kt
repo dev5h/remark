0184ffa7f6fbf7eb7a1c339dfa858ac048375b91
Shazin <shazin.bd@gmail.com>
2024-03-19 22:20:39 +0600

    Updated app/src/main/java/com/shazin/remark/utils.kt;

--- app/src/main/java/com/shazin/remark/utils.kt
+++ app/src/main/java/com/shazin/remark/utils.kt
@@ -1,18 +1,29 @@
 package com.shazin.remark
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 
+@RequiresApi(Build.VERSION_CODES.O)
+fun getColorHex(color: Color): String {
+    val argb = color.toArgb()
+    return String.format("#%08X", argb)
+}
+
 fun replaceToken(source: String, token: String, replacement: String): String {
     return source.replace(token, replacement)
 }
 
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun getPreviewTemplate(context: Context){
     val input = context.assets.open("html/index.html")
@@ -23,6 +34,6 @@ fun getPreviewTemplate(context: Context){
         stringBuilder.append(line)
     }
     var string  = stringBuilder.toString()
-    string  = string.replace("bgColor", MaterialTheme.colorScheme.background.toArgb().toString())
+    string  = string.replace("bgColor", getColorHex(MaterialTheme.colorScheme.background))
     println(string)
 }
