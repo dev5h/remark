a4f0d3ade680dc98cbd678573a4d7fb4be7f932b
Shazin <shazin.bd@gmail.com>
2024-03-19 22:15:28 +0600

    Updated app/src/main/assets/html/index.html, app/src/main/java/com/shazin/remark/utils.kt;

--- app/src/main/java/com/shazin/remark/utils.kt
+++ app/src/main/java/com/shazin/remark/utils.kt
@@ -1,11 +1,19 @@
 package com.shazin.remark
 
 import android.content.Context
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.toArgb
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 
+fun replaceToken(source: String, token: String, replacement: String): String {
+    return source.replace(token, replacement)
+}
+
+@Composable
 fun getPreviewTemplate(context: Context){
     val input = context.assets.open("html/index.html")
     val bufferedReader = BufferedReader(InputStreamReader(input))
@@ -14,5 +22,7 @@ fun getPreviewTemplate(context: Context){
     while (bufferedReader.readLine().also { line = it } != null) {
         stringBuilder.append(line)
     }
-    println(stringBuilder)
+    var string  = stringBuilder.toString()
+    string  = string.replace("bgColor", MaterialTheme.colorScheme.background.toArgb().toString())
+    println(string)
 }
