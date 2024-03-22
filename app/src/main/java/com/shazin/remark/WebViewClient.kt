961e50c40b24e7c596fdbb4ad81590c539ede941
Shazin <shazin.bd@gmail.com>
2024-03-23 02:27:46 +0600

    Added app/src/main/java/com/shazin/remark/WebViewClient.kt;

---
+++ app/src/main/java/com/shazin/remark/WebViewClient.kt
@@ -0,0 +1,29 @@
+package com.shazin.remark
+
+import android.graphics.Bitmap
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MyWebViewClient : WebViewClient() {
+    override fun shouldOverrideUrlLoading(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): Boolean {
+        // This method is called when a new URL is about to be loaded in the WebView.
+        // You can perform actions here, such as displaying a loading indicator.
+        return super.shouldOverrideUrlLoading(view, request)
+    }
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        // This method is called when the WebView starts to load a new page.
+        // You can perform actions here, such as displaying a loading indicator.
+        super.onPageStarted(view, url, favicon)
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        // This method is called when the WebView finishes loading a page.
+        // You can perform actions here, such as hiding a loading indicator.
+        super.onPageFinished(view, url)
+    }
+}
