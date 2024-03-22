e09ca7d97919aafb7fa8b896373d252fd0d19cc3
Shazin <shazin.bd@gmail.com>
2024-03-23 03:58:42 +0600

    Updated app/src/main/assets/html/index.html, app/src/main/assets/html/script.js, app/src/main/assets/html/style.css;

--- app/src/main/assets/html/script.js
+++ app/src/main/assets/html/script.js
@@ -3,7 +3,21 @@ const md = window.markdownit();
 
 document.addEventListener("DOMContentLoaded", function () {
   const md = window.markdownit();
-  container.innerHTML = md.render(container.innerText);
+  const test = `
+  # Hello this is some markdown texts
+  Hello world lamo here's a math formula for ya
+  ### Hello World I am Shazin 
+  Some text with KaTeX syntax: $ E = mc^2 $ and $F = ma$ 
+  $x^3$ 
+  $$\\int_0^{\\pi/2}  sin^mx+ cos^nx dx$$ 
+  Laplacian Transform Well to Do So we do something like below Hello I am
+
+  shazin and you are watching 9xe Hahahaah I and love to do stuffs as well
+  lamo
+  ## Code Snippet Samples
+  Hello
+  `;
+  container.innerHTML = md.render(test);
   // container.innerHTML = marked.parse(container.innerHTML);
   renderMathInElement(container, {
     // customised options
