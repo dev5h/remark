c04a7c7de890ee285c967710096df5492758c22f
Shazin <shazin.bd@gmail.com>
2024-03-25 04:24:01 +0600

    Updated app/src/main/java/com/shazin/remark/DataStore.kt;

--- app/src/main/java/com/shazin/remark/DataStore.kt
+++ app/src/main/java/com/shazin/remark/DataStore.kt
@@ -1,6 +1,8 @@
 package com.shazin.remark
 
+import android.content.Context
 import com.google.gson.Gson
+import java.io.File
 
 
 data class Note(
@@ -12,4 +14,16 @@ data class Note(
 fun serializeNoteFile(note: Note): String{
     val gson = Gson()
     return gson.toJson(note)
+}
+
+class DataStore(
+    val context: Context
+){
+    private fun saveLocal(uuid: String, json: String){
+        val path = context.dataDir.path.plus("${uuid}.db")
+        File(path).writeText(json)
+    }
+    fun saveNote(note: Note){
+        saveLocal(note.uuid, serializeNoteFile(note))
+    }
 }
